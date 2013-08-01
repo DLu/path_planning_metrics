80f73ba3f8f856510d489300c32cee2c699bd364
David Lu <davidvlu@gmail.com>
2013-08-01 15:15:07 -0500

    People Publisher

---
+++ path_planning_simulation/src/people_publisher.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import roslib; roslib.load_manifest('path_planning_simulation')
+import rospy
+import gazebo_msgs.msg
+from people_msgs.msg import People, Person
+
+class PeoplePublisher:
+    def __init__(self):
+        rospy.init_node('people_publisher')
+        self.names = rospy.get_param('/nav_experiments/people_names', [])
+        
+        self.sub = rospy.Subscriber('/simulation_state', gazebo_msgs.msg.ModelStates, self.model_state_cb)
+        self.pub = rospy.Publisher('/people', People)
+
+    def model_state_cb(self, msg):
+        people_list = People()
+        people_list.header.stamp = rospy.Time.now()
+        people_list.header.frame_id = '/map'
+        for name, pose, twist in zip(msg.name, msg.pose, msg.twist):
+            if name not in self.names:
+                continue
+                
+            p = Person()
+            p.name = name
+            p.position = pose.position
+            p.velocity.x  = twist.linear.x
+            p.velocity.y  = twist.linear.y
+            p.velocity.z  = twist.linear.z
+            p.reliability = 1.0
+            people_list.people.append(p)
+        self.pub.publish(people_list)
+
+if __name__=='__main__':
+    pp = PeoplePublisher()
+    rospy.spin()
+    
