7f8a3fe0497fe19a3f3ef85d1ce07d1157e5e8e4
David Lu!! <davidvlu@gmail.com>
2013-08-20 20:46:25 -0500

    Batch Batch Trials

--- path_planning_ruler/src/batch_trials.py
+++ path_planning_ruler/src/batch_trials.py
@@ -51,29 +51,16 @@ def multiply(parameterizations, name, val_str):
             newm.update(p)
             newp.append(newm)
     return newp
-    
-if __name__=='__main__':
-    rospy.init_node('batch_trials_script')
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('algorithm', metavar='algorithm.cfg')
-    parser.add_argument('scenarios', metavar='scenario.yaml', nargs='+')
-    parser.add_argument("-n", "--num_trials", dest="n", help='Number of trials per configuration', metavar='N', type=int, default=10)
-    parser.add_argument('--clean', action='store_true')
-    parser.add_argument('--var1', nargs=2)
-    parser.add_argument('--var2', nargs=2)
-    parser.add_argument('-q', '--quiet', action='store_true')
-
-    args = parser.parse_args()
-    scenarios = [Scenario(filename) for filename in args.scenarios]
+def parameterize(var1, var2):
     parameterizations = [{}]
 
-    if args.var1:
-        param1, N_str = args.var1
+    if var1:
+        param1, N_str = var1
         parameterizations = multiply(parameterizations, param1, N_str)
 
-        if args.var2:
-            param2, N_str = args.var2
+        if var2:
+            param2, N_str = var2
             key1, key2 = param_keys(param1, param2)
             parameterizations = multiply(parameterizations, param2, N_str)
             directory = '%(root)s/twoD/%(algorithm)s-%(key1)s-%(key2)s'
@@ -86,13 +73,18 @@ if __name__=='__main__':
         directory = '%(root)s/core'
         pattern = '%(scenario_key)s-%(algorithm)s-%%03d.bag'
 
-    m = MoveBaseInstance(quiet=args.quiet)
+    return parameterizations, directory, pattern, param1, key1, param2, key2
+
+
+def run_one_set(algorithm_fn, scenarios, n, parameterizations, directory, pattern, param1, key1, param2, key2, clean, quiet):
+    m = MoveBaseInstance(quiet=quiet)
+    scenarios = [Scenario(filename) for filename in scenarios]
 
     for parameterization in parameterizations:
-        values = m.configure(args.algorithm, parameterization)
-        if args.var1:
+        values = m.configure(algorithm_fn, parameterization)
+        if param1:
             value1 = values[param1]
-        if args.var2:
+        if param2:
             value2 = values[param2]
         if len(parameterizations)>0:
             s = ', '.join(['%s: %s'%(str(k),str(v)) for k,v in values.iteritems()])
@@ -110,8 +102,28 @@ if __name__=='__main__':
                 
                 mkdir_p(thedir)
                 full_pattern = '%s/%s'%(thedir, thepattern )
-                run_batch_scenario(scenario, args.n, full_pattern, args.clean, args.quiet)
+                run_batch_scenario(scenario, n, full_pattern, clean, quiet)
         finally:
             m.shutdown()
+
+
+    
+if __name__=='__main__':
+    rospy.init_node('batch_trials_script')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('algorithm', metavar='algorithm.cfg')
+    parser.add_argument('scenarios', metavar='scenario.yaml', nargs='+')
+    parser.add_argument("-n", "--num_trials", dest="n", help='Number of trials per configuration', metavar='N', type=int, default=10)
+    parser.add_argument('--clean', action='store_true')
+    parser.add_argument('--var1', nargs=2)
+    parser.add_argument('--var2', nargs=2)
+    parser.add_argument('-q', '--quiet', action='store_true')
+
+    args = parser.parse_args()
+
+    directory, pattern, param1, key1, param2, key2 = parameterize(args.var1, args.var2)
+    run_one_set(args.algorithm, args.scenarios, args.n, parameterizations, directory, pattern, param1, key1, param2, key2, args.clean, args.quiet)
+
 
 
