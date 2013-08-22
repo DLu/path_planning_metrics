63097752b656810864cf27b940e574252ddefc55
David Lu!! <davidvlu@gmail.com>
2013-08-22 16:15:25 -0500

    Update size draw

--- path_planning_analysis/src/size_draw.py
+++ path_planning_analysis/src/size_draw.py
@@ -3,21 +3,30 @@
 import roslib; roslib.load_manifest('path_planning_analysis')
 import sys
 from path_planning_analysis import *
+from path_planning_analysis.interactive import *
 import pylab
 
 if __name__=='__main__':
     ax = pylab.axes()
     pylab.axis('equal')
-    path = RobotPath(sys.argv[1])  
-    
-    x =[]
-    y =[]
-    for t, pose in path.poses:
-        x.append(pose.x)
-        y.append(pose.y)
-    curvatures = path.get_curvatures(5,2)
 
-    ax.scatter(x,y,s=[30*a for a in curvatures])
+    headers, bags = analysis_argparse(one=True, points=False, arrays=True)
+    header = headers[0]
+
+    for bagfn in bags:
+        path = PathStats(bagfn)
+        path.load(True) 
+    
+        x =[]
+        y =[]
+        for pose in path.poses:
+            x.append(pose.x)
+            y.append(pose.y)
+
+        values = getattr(path, header)
+        print min(values), max(values)
+
+        ax.scatter(x,y,s=[30*a/max(values) for a in values])
 
     pylab.show()
 
