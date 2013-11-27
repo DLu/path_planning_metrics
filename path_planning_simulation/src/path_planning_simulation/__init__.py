9deeef8e5ec19976bf56941f88bd2d98a100a01d
David Lu <davidvlu@gmail.com>
2013-11-27 10:11:11 -0600

    Get Models and Spawn Robot

--- path_planning_simulation/src/path_planning_simulation/__init__.py
+++ path_planning_simulation/src/path_planning_simulation/__init__.py
@@ -1,6 +1,6 @@
 import roslib; roslib.load_manifest('path_planning_simulation')
 import rospy
-from gazebo_msgs.srv import SetModelState, SpawnModel, DeleteModel
+from gazebo_msgs.srv import SetModelState, SpawnModel, DeleteModel, GetWorldProperties
 from gazebo_msgs.msg import ModelState
 from geometry_msgs.msg import Pose
 from tf.transformations import quaternion_from_euler
@@ -9,6 +9,7 @@ from geometry_msgs.msg import Pose2D
 SET_STATE_NAME = '/gazebo/set_model_state'
 SPAWN_NAME = '/gazebo/spawn_gazebo_model'
 DELETE_NAME = '/gazebo/delete_model'
+WORLD_NAME = '/gazebo/get_world_properties'
 
 def get_pose(x, y, theta):
     p = Pose()
@@ -36,15 +37,16 @@ def get_pose2d(scenario, key):
 def pose2d_to_pose(pose):
     return get_pose(pose.x, pose.y, pose.theta)
     
+def service(name, s_type):
+    rospy.wait_for_service(name)
+    return rospy.ServiceProxy(name, s_type)    
 
 class GazeboHelper:
     def __init__(self, quiet=False):
-        rospy.wait_for_service(SET_STATE_NAME)
-        self.state_proxy = rospy.ServiceProxy(SET_STATE_NAME, SetModelState)
-        rospy.wait_for_service(SPAWN_NAME)
-        self.spawn_proxy = rospy.ServiceProxy(SPAWN_NAME, SpawnModel)
-        rospy.wait_for_service(DELETE_NAME)
-        self.delete_proxy = rospy.ServiceProxy(DELETE_NAME, DeleteModel)
+        self.state_proxy = service(SET_STATE_NAME, SetModelState)
+        self.spawn_proxy = service(SPAWN_NAME, SpawnModel)
+        self.delete_proxy = service(DELETE_NAME, DeleteModel)
+        self.world_proxy = service(WORLD_NAME, GetWorldProperties)
         self.quiet = quiet
 
     def set_state(self, name, pose, twist=None, frame='/map'):
@@ -58,9 +60,9 @@ class GazeboHelper:
         if not self.quiet:
             rospy.loginfo("SetState: %s | %s", "Success" if response.success else "Failure", response.status_message)
             
-    def spawn_robot(self):
+    def spawn_robot(self, name):
         description = rospy.get_param('/robot_description')
-        self.spawn_model('pr2', description, Pose2D(0,0,0))
+        self.spawn_model(name, description, Pose2D(0,0,0))
 
     def spawn_model(self, name, xml, pose, frame='/map', namespace=""):
         response = self.spawn_proxy(name, xml, namespace, pose, frame)
@@ -71,5 +73,16 @@ class GazeboHelper:
         response = self.delete_proxy(name)
         if not self.quiet:
             rospy.loginfo("DeleteModel: %s | %s", "Success" if response.success else "Failure", response.status_message)
+            
+    def get_models(self):
+        response = self.world_proxy()
+        return response.model_names
+        
+    def spawn_robot_maybe(self, name='pr2'):
+        models = self.get_models()
+        if name in models:
+            return
+        else:
+            self.spawn_robot(name)
 
 
